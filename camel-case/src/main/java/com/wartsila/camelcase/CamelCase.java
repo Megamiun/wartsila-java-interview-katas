75b1b6362fb6639a37c804f347e0122b8bf594f1
gabryel.monteiro <gabryelarm@gmail.com>
2023-02-03 14:53:54 +0100

    Solving problem using Regex and creating more test cases

--- camel-case/src/main/java/com/wartsila/camelcase/CamelCase.java
+++ camel-case/src/main/java/com/wartsila/camelcase/CamelCase.java
@@ -1,8 +1,15 @@
 package com.wartsila.camelcase;
 
+import java.util.regex.Pattern;
+
 class CamelCase {
 
+    private final static Pattern separatorFinder = Pattern.compile("[-_](.)");
+
     static String toCamelCase(String s){
-        return "";
+        if (s == null) return null;
+
+        var cleanString = s.replaceAll("\\s+", "");
+        return separatorFinder.matcher(cleanString).replaceAll(matchResult -> matchResult.group(1).toUpperCase());
     }
 }
--- camel-case/src/test/java/com/wartsila/camelcase/CamelCaseTest.java
+++ camel-case/src/test/java/com/wartsila/camelcase/CamelCaseTest.java
@@ -3,6 +3,7 @@ package com.wartsila.camelcase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CamelCaseTest {
     @Test
@@ -17,5 +18,47 @@ public class CamelCaseTest {
         String input = "digital-Product-Development";
         System.out.println("input: " + input);
         assertEquals("digitalProductDevelopment", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testSomeDashLowerInTheMiddle_success() {
+        String input = "digital-product-development";
+        System.out.println("input: " + input);
+        assertEquals("digitalProductDevelopment", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testNull_shouldReturnNull() {
+        String input = null;
+        System.out.println("input: " + input);
+        assertNull(CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testEmpty_shouldReturnSame() {
+        String input = "digital-product-development";
+        System.out.println("input: " + input);
+        assertEquals("digitalProductDevelopment", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testBlank_shouldDropSpaces() {
+        String input = "                  ";
+        System.out.println("input: " + input);
+        assertEquals("", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testStringWithSpaces_shouldDropSpaces() {
+        String input = "         a-cool-test         ";
+        System.out.println("input: " + input);
+        assertEquals("aCoolTest", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testStringWithSpacesInTheMiddle_shouldDropSpaces() {
+        String input = "         a cool test         ";
+        System.out.println("input: " + input);
+        assertEquals("acooltest", CamelCase.toCamelCase(input));
     }
 }
